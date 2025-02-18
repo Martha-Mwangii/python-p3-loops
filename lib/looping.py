d25d4887af126625178b121d777bbd096eb1a833
Martha Wangui Mwangi <marthawm7252@gmail.com>
2025-02-18 09:25:34 +0300

    python-p3-loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+      
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_numbers = [num ** 2 for num in int_list]
+    return squared_numbers
+
 
 def fizzbuzz():
+     for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
     # code goes here!
-    pass
+   
